c883745280ca2fbdbc4cfe0fee35c5063d91743b
master <kim.dhdbr@gmail.com>
2017-08-15 21:49:59 +0900

    Add library & write import & Error log4j:WARN No appenders could be found for logger (mr.huntlab.MainLog4j). log4j:WARN Please initialize the log4j system properly. log4j:WARN See http://logging.apache.org/log4j/1.2/faq.html#noconfig for more info.

--- src/mr/huntlab/MainLog4j.java
+++ src/mr/huntlab/MainLog4j.java
@@ -1,9 +1,19 @@
 package mr.huntlab;
 
+import org.apache.log4j.*;
+
 public class MainLog4j {
 	
 	public static void main(String[] args) {
 		
+		Logger logger = Logger.getLogger(MainLog4j.class);
+		
+		logger.debug("MainLog4j main class debug message.");
+		logger.error("MainLog4j main class error message.");
+		logger.info("MainLog4j main class info message.");
+		logger.fatal("MainLog4j main class fatal message.");
+		logger.trace("MainLog4j main class trace message.");
+		logger.warn("MainLog4j main class warn message.");
 	}
 
 }
